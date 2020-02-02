2b540beee7ff2c8a9df340a50c2f819a36fd7849
haikubabe <sreech94@gmail.com>
2020-02-02 22:29:07 +0530

    Add Selection Sort

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -1,4 +1,32 @@
 package sorting;
 
 public class SelectionSort {
+
+    private static int[] sort(int[] a, int n) {
+        for (int i=0;i<n-1;i++) {
+            int min = a[i], minIdx = i;
+            for (int j=i+1;j<n;j++) {
+                if (a[j]<min) {
+                    min = a[j];
+                    minIdx = j;
+                }
+            }
+            if (min != a[i]) {
+                int t = a[i];
+                a[i] = min;
+                a[minIdx] = t;
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {6,2,8,0,1,4,3};
+        int n = a.length;
+        int[] b = sort(a, n);
+        for (int i=0;i<n;i++) {
+            System.out.printf("%d\t", b[i]);
+        }
+        System.out.println();
+    }
 }

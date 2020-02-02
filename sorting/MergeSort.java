7f432ff08b1c2bf1d0e8079bfa1c44f561f7603d
haikubabe <sreech94@gmail.com>
2020-02-02 22:28:31 +0530

    Add Merge Sort

--- sorting/MergeSort.java
+++ sorting/MergeSort.java
@@ -1,4 +1,56 @@
 package sorting;
 
 public class MergeSort {
+    private static void merge(int[] A, int p, int q, int r) {
+        int nL = q - p + 1;
+        int nR = r - q;
+        int[] L = new int[nL];
+        int[] R = new int[nR];
+        for (int i = 0; i < nL; i++) {
+            L[i] = A[i + p];
+        }
+        for (int j = 0; j < nR; j++) {
+            R[j] = A[j + q + 1];
+        }
+        int i = 0, j = 0, k = p;
+        while (i < nL && j < nR) {
+            if (L[i] <= R[j]) {
+                A[k] = L[i];
+                i++;
+            } else {
+                A[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < nL) {
+            A[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < nR) {
+            A[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    private static void mergeSort(int[] A, int p, int r) {
+        if (p < r) {
+            int q = (p + r) / 2;
+            mergeSort(A, p, q);
+            mergeSort(A, q + 1, r);
+            merge(A, p, q, r);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5,4,1,8,7,2,6,3};
+        int n = arr.length;
+        mergeSort(arr, 0, n - 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
+
